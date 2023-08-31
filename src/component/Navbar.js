34a213201fce9dd862c4f34c44d02dd377715144
Patel Harsh <102842153+Harrshhpattell@users.noreply.github.com>
2023-08-31 23:28:42 +0530

    complete navbar

---
+++ src/component/Navbar.js
@@ -0,0 +1,14 @@
+export default function Navbar() {
+  return (
+    <div className="navbar">
+      <h1>Currency Converter 💱 </h1>
+      <a
+        href="https://github.com/Harrshhpattell"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <button>Follow Me</button>
+      </a>
+    </div>
+  );
+}
