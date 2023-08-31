a0c591bcee906b35e8858309e45625756ae62340
Patel Harsh <102842153+Harrshhpattell@users.noreply.github.com>
2023-09-01 01:38:10 +0530

    update

--- src/App.js
+++ src/App.js
@@ -11,7 +11,31 @@ function App() {
 }
 
 function Main() {
-  return <p>hello</p>;
+  return (
+    <>
+      <div className="main">
+        <div className="heading">
+          <div className="main-head">
+            <p id="first">Currency</p>
+
+            <p id="sec">Converter</p>
+          </div>
+
+          <p>
+            Explore Global Currencies at a Glance: Real-Time Updates for Every
+            Country's Exchange Rate.
+          </p>
+          <span>
+            <img src="./images/money.jpg" alt="money icon" />
+          </span>
+        </div>
+        <div className="icon">
+          <img src="./images/converter.jpg" alt="converter icon" />
+        </div>
+      </div>
+      <hr />
+    </>
+  );
 }
 
 export default App;
